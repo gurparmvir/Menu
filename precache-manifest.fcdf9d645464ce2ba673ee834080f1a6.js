faca3da4c127f764421ecff46582fffc4136fc16
Gurparmvir Singh <gurparmvirsingh@Gurparmvirs-Mac-mini.local>
2020-12-21 22:18:20 -0500

    Updates

---
+++ precache-manifest.fcdf9d645464ce2ba673ee834080f1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caca1a8606caf4701b49d955d0f3fb9",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "eb50c2fff6ef110d77b8",
+    "url": "/menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb50c2fff6ef110d77b8",
+    "url": "/menu/static/js/main.00be43d3.chunk.js"
+  },
+  {
+    "revision": "56ef0208991cca3c548d",
+    "url": "/menu/static/js/runtime-main.a5a74f46.js"
+  }
+]);
